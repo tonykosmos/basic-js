515471db551c3a3d6a9640116b810baefef02ef6
tonykosmos <a.kosmovskiy@gmail.com>
2020-10-05 14:21:10 +0300

    feat: add slution of recursive-depth task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
+  arrayCounter = 1;
+  maxDeep = 1;
   calculateDepth(arr) {
-    throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+    for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i])) {
+        this.arrayCounter++;
+        this.calculateDepth(arr[i]);
+        this.maxDeep = Math.max(this.maxDeep, this.arrayCounter);
+        if (i == arr.length - 1 || Array.isArray(arr[i])) this.arrayCounter = 1;  
+      }  
+      }
+    return this.maxDeep;
   }
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+ 
+  encrypt(message, key) {
+    let alphabet = "abcdefghijklmnopqrstuvwxyz";
+    let spacePositions = [];
+    let encrypted = "";
+    
+    for (let i = 0; i < message.length; i++) {
+      if (/[0-9,.-:;'"`_!?]/.test(message[i])) continue;
+      if (message[i] == " ") encrypted += " ";
+      
+      let newLetter = alphabet.indexOf(message[i]) + alphabet.indexOf(key[i]);
+      if (newLetter > 26) newLetter = newLetter - 26;
+      encrypted += alphabet[newLetter];
+    }
+    return encrypted.toUpperCase();
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(message, key) {
+    let alphabet = "abcdefghijklmnopqrstuvwxyz";
+    let spacePositions = [];
+    for (let i = 0; i < message.length; i++) {
+      if (message[i] == " ") spacePositions.push(i);
+    }
+    return spacePositions;
   }
 }
 
