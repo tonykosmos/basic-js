f32dfe2b85b882554bdfd1e7eb1675c6688b7307
tonykosmos <a.kosmovskiy@gmail.com>
2020-10-05 22:44:50 +0300

    feat: add part of extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
-  throw new CustomError('Not implemented');
+  let newStr = "";
+  if (options.repeatTimes) {
+   for (let i = 1; i <= options.repeatTimes; i++) {
+    if (options.separator && options.addition) {
+      newStr += `${options.separator}${str}${options.addition}`;
+    } 
+    else if (options.separator) {
+      newStr += `${options.separator}${str}`;
+    }
+    else newStr += `+${str}`;
+  }
+}
+
+  else if (options.addition) {
+    newStr += `${str}${options.addition}`;
+    return newStr;
+  }
   
+
+
+  if (options.separator) {
+    return newStr.slice((options.separator).length)
+  }
+  else return newStr.slice(1);
 };
   
