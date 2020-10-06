3f30551ef6ce27600d2e2828d4f2198446c587a7
tonykosmos <a.kosmovskiy@gmail.com>
2020-10-06 17:55:02 +0300

    feat: add extended repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,10 +2,20 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
   let newStr = "";
+  let repeatedAddition = "";
+  let finalAddition = "";
+  for (let i = 1; i <= options.additionRepeatTimes; i++) {
+    if (options.additionSeparator) {
+      repeatedAddition += `${options.addition}${options.additionSeparator}`;
+      finalAddition = repeatedAddition.slice(0, repeatedAddition.length - (options.additionSeparator).length);
+    }
+    else finalAddition += `${options.addition}`;
+  }
+
   if (options.repeatTimes) {
    for (let i = 1; i <= options.repeatTimes; i++) {
     if (options.separator && options.addition) {
-      newStr += `${options.separator}${str}${options.addition}`;
+      newStr += `${options.separator}${str}${finalAddition}`;
     } 
     else if (options.separator) {
       newStr += `${options.separator}${str}`;
@@ -22,7 +32,7 @@ module.exports = function repeater(str, options) {
 
 
   if (options.separator) {
-    return newStr.slice((options.separator).length)
+   return newStr.slice((options.separator).length)
   }
   else return newStr.slice(1);
 };
