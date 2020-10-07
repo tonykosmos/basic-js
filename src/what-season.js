ace6d5d21de0544f3ff84d0ab5b75a6fde976b42
tonykosmos <a.kosmovskiy@gmail.com>
2020-10-07 17:22:23 +0300

    feat: add check of date object in what season task

--- src/what-season.js
+++ src/what-season.js
@@ -4,6 +4,7 @@ module.exports = function getSeason(date) {
   
   let month = date.getMonth() + 1 ;
   if (arguments.length == 0)  return 'Unable to determine the time of year!'; 
+  if (date instanceof Date == false || {}.toString.call(date) !== "[object Date]") throw new Error();
   if ((month>=1 && month <=2) || month == 12) return "winter";
   if (month>=3 && month <=5) return "spring";
   if (month>=6 && month <=8) return "summer";
