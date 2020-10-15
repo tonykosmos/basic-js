1f52f721e2d48f355f5274b8b835d10085b904b1
tonykosmos <a.kosmovskiy@gmail.com>
2020-10-16 02:23:13 +0300

    fix: resolve recursive depth task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,17 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  arrayCounter = 1;
-  maxDeep = 1;
+  
   calculateDepth(arr) {
+    let maxDepth = 1;
+    let currentDepth = 1;
     for (let i = 0; i < arr.length; i++) {
       if (Array.isArray(arr[i])) {
-        this.arrayCounter++;
-        this.calculateDepth(arr[i]);
-        this.maxDeep = Math.max(this.maxDeep, this.arrayCounter);
-        if (i == arr.length - 1 || Array.isArray(arr[i])) this.arrayCounter = 1;  
-      }  
+        currentDepth += this.calculateDepth(arr[i]);
+        }
+        if (currentDepth > maxDepth) {
+          maxDepth = currentDepth;
+        }
+        currentDepth = 1;
       }
-    return this.maxDeep;
+    return maxDepth;
   }
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,9 +1,8 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
-  
+  if (arguments.length === 0)  return 'Unable to determine the time of year!'; 
   let month = date.getMonth() + 1 ;
-  if (arguments.length == 0)  return 'Unable to determine the time of year!'; 
   if (date instanceof Date == false || {}.toString.call(date) !== "[object Date]") throw new Error();
   if ((month>=1 && month <=2) || month == 12) return "winter";
   if (month>=3 && month <=5) return "spring";
